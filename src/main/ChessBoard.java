d9a0d9609a697348e3a720f77b48c429e993df9a
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-24 16:57:36 -0300

    create sigleton pattern

--- src/main/ChessBoard.java
+++ src/main/ChessBoard.java
@@ -1,5 +1,13 @@
 package main;
 
 public class ChessBoard {
-
+	static private ChessBoard instance =  new ChessBoard();
+	
+	private ChessBoard() {}
+	
+	static ChessBoard getInstance () {return instance;}
+	
+	public static void main(String[] args) {
+		ChessBoard tabuleiro = ChessBoard.getInstance();
+	}
 }
