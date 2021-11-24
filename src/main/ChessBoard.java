196bd9e827f49938335e0c8e8a18820882ccdf11
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2021-11-24 17:01:28 -0300

    add lazy instance

--- src/main/ChessBoard.java
+++ src/main/ChessBoard.java
@@ -1,11 +1,17 @@
 package main;
 
 public class ChessBoard {
-	static private ChessBoard instance =  new ChessBoard();
+	static private ChessBoard instance = null;
 	
 	private ChessBoard() {}
 	
-	static ChessBoard getInstance () {return instance;}
+	static ChessBoard getInstance () {
+		if( instance == null) {
+			instance = new ChessBoard();
+		}
+		
+		return instance;
+	}
 	
 	public static void main(String[] args) {
 		ChessBoard tabuleiro = ChessBoard.getInstance();
